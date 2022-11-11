b61a2cb61c6812425902d6459f82d5f3678b3df0
LMJW <heysuperming@gmail.com>
2022-11-11 14:47:41 +1100

    Reuse the single instance of the handler?

--- examples/echo/src/bin/quic_echo_server.rs
+++ examples/echo/src/bin/quic_echo_server.rs
@@ -4,8 +4,16 @@
 use s2n_quic::Server;
 use std::error::Error;
 use s2n_quic::provider::tls::s2n_tls::{self as s2n_quic_tls, ClientHelloCallback, Connection, ConfigLoader, ConnectionContext};
-use core::{task::Poll, sync::atomic::{AtomicBool, Ordering}};
-use std::sync::Arc;
+use core::{task::{Context, Poll, Waker}, sync::atomic::{AtomicBool, Ordering}};
+use std::{
+    future::Future,
+    pin::Pin,
+    sync::{Arc, Mutex},
+    thread,
+    time::Duration,
+};
+use std::ops::DerefMut;
+
 
 /// NOTE: this certificate is to be used for demonstration purposes only!
 pub static CERT_PEM: &str = include_str!(concat!(
@@ -18,26 +26,103 @@ pub static KEY_PEM: &str = include_str!(concat!(
     "/../../quic/s2n-quic-core/certs/key.pem"
 ));
 
-#[derive(Clone)]
-pub struct MyClientHelloHandler{}
+pub struct MyClientHelloHandler{
+    fut: std::cell::RefCell<Option<Pin<Box<MyTimeoutFuture>>>>,
+}
+
+
+impl MyClientHelloHandler{
+    fn new() -> Self{
+        Self{
+            fut: std::cell::RefCell::new(None),
+        }
+    }
+}
+
+pub struct MyTimeoutFuture{
+    shared_state: Arc<Mutex<SharedState>>,
+}
+
+struct SharedState{
+    completed: bool,
+    waker: Option<Waker>,
+}
+
+impl Future for MyTimeoutFuture {
+    type Output = Result<(Vec<u8>, Vec<u8>), ()>;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let mut shared_state = self.shared_state.lock().unwrap();
+        if shared_state.completed {
+            Poll::Ready(Ok((vec![], vec![])))
+        }else {
+            shared_state.waker = Some(cx.waker().clone());
+            Poll::Pending
+        }
+    }
+}
+
+impl MyTimeoutFuture {
+    pub fn new(duration: Duration) -> Self {
+        let shared_state = Arc::new(Mutex::new(SharedState {
+            completed: false,
+            waker: None,
+        }));
+
+        // Spawn the new thread
+        let thread_shared_state = shared_state.clone();
+        tokio::spawn(async move {
+            println!("sleep started");
+            tokio::time::sleep(duration).await;
+            println!("sleep completed");
+            let mut shared_state = thread_shared_state.lock().unwrap();
+            // Signal that the timer has completed and wake up the last
+            // task on which the future was polled, if one exists.
+            shared_state.completed = true;
+            if let Some(waker) = shared_state.waker.take() {
+                waker.wake()
+            }
+        });
+
+        Self { shared_state }
+    }
+}
 
 impl ClientHelloCallback for MyClientHelloHandler {
     fn poll_client_hello(&self, conn: &mut Connection) -> core::task::Poll<Result<(),s2n_tls::error::Error>> {
-        let mut config = s2n_tls::config::Builder::new();
+        // println!("Poll MyClientHelloHandler..");
+        let waker = conn.waker().unwrap();
+        let mut ctx = Context::from_waker(waker);
 
-        config.enable_quic().unwrap(); 
-        config.set_security_policy(&s2n_tls::security::DEFAULT_TLS13).unwrap(); 
-        config.set_application_protocol_preference([b"h3"]).unwrap(); 
-        config.set_client_hello_callback(self.clone());
-
-        config.load_pem(CERT_PEM.as_bytes(), KEY_PEM.as_bytes()).unwrap();
-        let config = config.build().unwrap();
-
-        conn.set_config(config).unwrap();
-
-        println!("Set config done");
-        conn.server_name_extension_used(); // If I use sni to config the connection, I need to call this.
-        Poll::Ready(Ok(()))
+        let mut fut = self.fut.borrow_mut();
+        if fut.is_none() {
+            fut.replace(Pin::new(Box::new(MyTimeoutFuture::new(Duration::from_secs(5)))));
+            Poll::Pending
+        }else {
+            let fut = fut.as_mut().unwrap();
+            let fut = fut.as_mut();
+            match fut.poll(&mut ctx){
+                Poll::Pending => Poll::Pending,
+                Poll::Ready(v) => {
+                    println!("{:?}", v);
+                    let mut config = s2n_tls::config::Builder::new();
+    
+                    config.enable_quic().unwrap(); 
+                    config.set_security_policy(&s2n_tls::security::DEFAULT_TLS13).unwrap(); 
+                    config.set_application_protocol_preference([b"h3"]).unwrap(); 
+                    //config.set_client_hello_callback(self.clone());
+            
+                    config.load_pem(CERT_PEM.as_bytes(), KEY_PEM.as_bytes()).unwrap();
+                    let config = config.build().unwrap();
+            
+                    conn.set_config(config).unwrap();
+            
+                    println!("Set config done");
+                    conn.server_name_extension_used(); // If I use sni to config the connection, I need to call this.
+                    Poll::Ready(Ok(()))
+                }
+            }    
+        }
     }
 }
 
@@ -46,7 +131,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let tls = s2n_quic::provider::tls::s2n_tls::Server::builder()
         //.with_certificate(CERT_PEM, KEY_PEM)?
         //.with_io("127.0.0.1:4433")?
-        .with_client_hello_handler(MyClientHelloHandler{})?
+        .with_client_hello_handler(MyClientHelloHandler::new())?
         .build()?;
 
     // let tls = s2n_quic::provider::tls::s2n_tls::Server::from_loader(MyLoader{});
