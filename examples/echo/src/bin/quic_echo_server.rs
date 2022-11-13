337c4dc766d886d754ab570dee50fe9b8c61d264
LMJW <heysuperming@gmail.com>
2022-11-13 16:47:31 +1100

    Implement with mutex

--- examples/echo/src/bin/quic_echo_server.rs
+++ examples/echo/src/bin/quic_echo_server.rs
@@ -6,14 +6,14 @@ use std::error::Error;
 use s2n_quic::provider::tls::s2n_tls::{self as s2n_quic_tls, ClientHelloCallback, Connection, ConfigLoader, ConnectionContext};
 use core::{task::{Context, Poll, Waker}, sync::atomic::{AtomicBool, Ordering}};
 use std::{
-    future::Future,
     pin::Pin,
     sync::{Arc, Mutex},
     thread,
-    time::Duration,
 };
 use std::ops::DerefMut;
-
+use tokio::time::{sleep, Duration};
+use futures::Future;
+use std::collections::HashMap;
 
 /// NOTE: this certificate is to be used for demonstration purposes only!
 pub static CERT_PEM: &str = include_str!(concat!(
@@ -26,23 +26,26 @@ pub static KEY_PEM: &str = include_str!(concat!(
     "/../../quic/s2n-quic-core/certs/key.pem"
 ));
 
+#[derive(Debug)]
 pub struct MyClientHelloHandler{
-    fut: std::cell::RefCell<Option<Pin<Box<MyTimeoutFuture>>>>,
+    state: Mutex<HashMap<String, Pin<Box<MyTimeoutFuture>>>>,
 }
 
 
 impl MyClientHelloHandler{
     fn new() -> Self{
         Self{
-            fut: std::cell::RefCell::new(None),
+            state: Mutex::new(HashMap::new()),
         }
     }
 }
 
+#[derive(Debug)]
 pub struct MyTimeoutFuture{
     shared_state: Arc<Mutex<SharedState>>,
 }
 
+#[derive(Debug)]
 struct SharedState{
     completed: bool,
     waker: Option<Waker>,
@@ -63,23 +66,25 @@ impl Future for MyTimeoutFuture {
 }
 
 impl MyTimeoutFuture {
-    pub fn new(duration: Duration) -> Self {
+    pub fn new(duration: Duration, waker: Option<Waker>) -> Self {
+        println!("Creating new MyTimeoutFuture");
         let shared_state = Arc::new(Mutex::new(SharedState {
             completed: false,
-            waker: None,
+            waker,
         }));
 
         // Spawn the new thread
         let thread_shared_state = shared_state.clone();
         tokio::spawn(async move {
-            println!("sleep started");
+            //println!("sleep started");
             tokio::time::sleep(duration).await;
-            println!("sleep completed");
+            //println!("sleep completed");
             let mut shared_state = thread_shared_state.lock().unwrap();
             // Signal that the timer has completed and wake up the last
             // task on which the future was polled, if one exists.
             shared_state.completed = true;
             if let Some(waker) = shared_state.waker.take() {
+                //println!("Call wake");
                 waker.wake()
             }
         });
@@ -90,38 +95,42 @@ impl MyTimeoutFuture {
 
 impl ClientHelloCallback for MyClientHelloHandler {
     fn poll_client_hello(&self, conn: &mut Connection) -> core::task::Poll<Result<(),s2n_tls::error::Error>> {
-        // println!("Poll MyClientHelloHandler..");
-        let waker = conn.waker().unwrap();
-        let mut ctx = Context::from_waker(waker);
+        //println!("Poll MyClientHelloHandler..");
 
-        let mut fut = self.fut.borrow_mut();
-        if fut.is_none() {
-            fut.replace(Pin::new(Box::new(MyTimeoutFuture::new(Duration::from_secs(5)))));
-            Poll::Pending
-        }else {
-            let fut = fut.as_mut().unwrap();
-            let fut = fut.as_mut();
-            match fut.poll(&mut ctx){
-                Poll::Pending => Poll::Pending,
+        if let Some(sni) = conn.server_name() {
+            let waker = conn.waker().unwrap();
+            let mut ctx = Context::from_waker(waker);
+            let mut map = self.state.lock().unwrap();
+
+            let mut fut = map.remove(sni).unwrap_or_else(||Box::pin(MyTimeoutFuture::new(Duration::from_secs(6), Some(waker.clone()))));
+
+            match fut.as_mut().poll(&mut ctx) {
+                Poll::Pending => {
+                    map.insert(sni.to_string(), fut);
+                    Poll::Pending
+                },
                 Poll::Ready(v) => {
-                    println!("{:?}", v);
+                    //println!("{:?}", v);
                     let mut config = s2n_tls::config::Builder::new();
-    
+
                     config.enable_quic().unwrap(); 
                     config.set_security_policy(&s2n_tls::security::DEFAULT_TLS13).unwrap(); 
                     config.set_application_protocol_preference([b"h3"]).unwrap(); 
                     //config.set_client_hello_callback(self.clone());
-            
+
                     config.load_pem(CERT_PEM.as_bytes(), KEY_PEM.as_bytes()).unwrap();
                     let config = config.build().unwrap();
-            
+
                     conn.set_config(config).unwrap();
-            
+
                     println!("Set config done");
-                    conn.server_name_extension_used(); // If I use sni to config the connection, I need to call this.
+                    conn.server_name_extension_used();
                     Poll::Ready(Ok(()))
                 }
-            }    
+            }
+
+        }else{
+            Poll::Ready(Err(todo!()))
         }
     }
 }
@@ -145,12 +154,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
     while let Some(mut connection) = server.accept().await {
         // spawn a new task for the connection
         tokio::spawn(async move {
-            eprintln!("Connection accepted from {:?}", connection.remote_addr());
+            // eprintln!("Connection accepted from {:?}", connection.remote_addr());
 
             while let Ok(Some(mut stream)) = connection.accept_bidirectional_stream().await {
                 // spawn a new task for the stream
                 tokio::spawn(async move {
-                    eprintln!("Stream opened from {:?}", stream.connection().remote_addr());
+                    // eprintln!("Stream opened from {:?}", stream.connection().remote_addr());
 
                     // echo any data back to the stream
                     while let Ok(Some(data)) = stream.receive().await {
